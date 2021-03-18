37aeef8794ed1a2f02aa0c69e791c71f10ccfc3b
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2021-03-18 21:14:23 +0300

    Versiyon açıklaması güncelleştirildi.

--- ChangeLog.cs
+++ ChangeLog.cs
@@ -16,5 +16,10 @@ namespace _345_Launcher
         {
             InitializeComponent();
         }
+
+        private void metroLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
